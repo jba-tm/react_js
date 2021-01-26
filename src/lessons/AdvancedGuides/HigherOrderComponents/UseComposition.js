533c01a591b7926196716ccfb9507032aa689107
jba-tm <jumageldiyewb@list.ru>
2021-01-26 12:47:34 +0500

    HOC

---
+++ src/lessons/AdvancedGuides/HigherOrderComponents/UseComposition.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function logProps(WrappedComponent) {
+    return class extends React.Component {
+        componentDidUpdate(prevProps) {
+            console.log('Current props: ', this.props);
+            console.log('Previous props: ', prevProps);
+        }
+        render() {
+            // Wraps the input component in a container, without mutating it. Good!
+            return <WrappedComponent {...this.props} />;
+        }
+    }
+}

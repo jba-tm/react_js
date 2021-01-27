3ebf10544a579e6d4bec6cd4bd7bad3dcd723eb9
jba-tm <jumageldiyewb@list.ru>
2021-01-27 11:19:39 +0500

    React without JSX

---
+++ src/lessons/AdvancedGuides/React Without JSX/WithoutJSX.js
@@ -0,0 +1,11 @@
+class Hello extends React.Component {
+    render() {
+        return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+    }
+}
+
+ReactDOM.render(
+    React.createElement(Hello, {toWhat: 'World'}, null),
+    document.getElementById('root')
+);
+
